29b0a5d98eff83c66d9a037479b4bfa4d328ed3c
barsadullahihsanzai-eng <barsadullahihsanzai@gmail.com>
2026-08-08 21:23:42 +0430

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,59 @@ function cutCake(){
     "🎂 The birthday cake is ready to share! 🎉";
 
 }
+
+// 🎁 Extra Surprise Effects
+
+function openGift(){
+
+    alert("🎁 A special surprise is waiting for Queen Ñajmusama ✨");
+
+}
+
+
+
+// 🎉 Confetti Effect
+
+function createConfetti(){
+
+    for(let i = 0; i < 40; i++){
+
+        let confetti = document.createElement("div");
+
+        confetti.innerHTML = "✨";
+
+        confetti.style.position = "fixed";
+        confetti.style.left = Math.random()*100 + "%";
+        confetti.style.top = "-20px";
+        confetti.style.fontSize = "25px";
+
+        document.body.appendChild(confetti);
+
+
+        let fall = setInterval(()=>{
+
+            confetti.style.top =
+            parseInt(confetti.style.top) + 5 + "px";
+
+
+            if(parseInt(confetti.style.top)>1000){
+
+                confetti.remove();
+                clearInterval(fall);
+
+            }
+
+        },30);
+
+    }
+
+}
+
+
+// Run magical effects when page loads
+
+window.onload = function(){
+
+    createConfetti();
+
+};
